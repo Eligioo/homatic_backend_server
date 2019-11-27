3f93c38d854158a8511d0961ae663fd01150fefb
Stefan Koolen <nast3zz@gmail.com>
2019-11-27 14:23:30 +0100

    Move portal routes to seperate file

---
+++ src/libs/express/api/v1/Portal.ts
@@ -0,0 +1,88 @@
+import express from "express";
+import { validationResult } from "express-validator";
+
+import Session from "../../../mongo/models/Session";
+import User from "../../../mongo/models/User";
+
+import ExpressValidator from "../../Validator";
+import Log from "../../../utils/Log";
+import Random from "../../../utils/Random";
+import bcrypt from "bcrypt";
+
+
+const router = express.Router();
+
+router.post("/user/login", ExpressValidator.UserLoginRoute, async (req:express.Request, res:express.Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(200).json({ error: errors.array() });
+  }
+
+  try {
+    const user = await User.findOne({
+      username: req.body.username
+    });
+  
+    if(user && await bcrypt.compare(req.body.password, user.password_hash)) {
+      const session = new Session({
+        key: Random.string(),
+        user: user._id,
+        ip_address: req.headers["x-forwarded-for"] || req.connection.remoteAddress
+      });
+
+      await session.save();
+      return res.json({key: session.key});
+    }
+    else {
+      return res.json({
+        error: [{
+          "msg": "Combinatie van gebruikersnaam en wachtwoord is onjuist."
+        }]
+      });
+    }
+
+  } catch (error) {
+    Log.error(`Mongo /user/login ${error.message}`);
+    return res.status(500).send();
+  }
+});
+
+router.post("/user/session", async (req, res) => {
+  try {
+    if(req.body.key) {
+      const session = await Session.findOne({key: req.body.key});
+      if(session && session.active && (req.headers["x-forwarded-for"] || req.connection.remoteAddress) === session.ip_address) {
+        session.last_activity = new Date(Date.now());
+        session.save();
+        return res.json("OK");
+      }
+      else {
+        throw Error("No valid session in database");
+      }
+    }
+    else {
+      throw Error("Session not in request");
+    }
+  } catch (error) {
+    Log.warn(`Invalid user session: ${error.message}`);
+    return res.json({error: {msg: "Ongeldige gebruikerssessie"}});
+  }
+});
+
+router.post("/user/logout", async (req, res) => {
+  if(req.body.key) {
+    try {
+      const session = await Session.findOne({key: req.body.key});
+      if(session && session.active && (req.headers["x-forwarded-for"] || req.connection.remoteAddress) === session.ip_address) {
+        session.active = false;
+        session.last_activity = new Date(Date.now());
+        session.save();
+        return res.json("OK");
+      }  
+    } catch (error) {
+      return res.json("OK");
+    }  
+  }
+});
+
+export default router;
